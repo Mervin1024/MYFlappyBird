b4e9025f68feeee73057e79ab0239092c418a462
马遥 <mervin1024@163.com>
2015-12-28 19:36:01 +0800

    scene

---
+++ MYFlappyBird/BackgroundSceneView.h
@@ -0,0 +1,13 @@
+//
+//  BackgroundSceneView.h
+//  MYFlappyBird
+//
+//  Created by 马遥 on 15/12/28.
+//  Copyright © 2015年 Mervin. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface BackgroundSceneView : NSObject
+
+@end
---
+++ MYFlappyBird/BackgroundSceneView.m
@@ -0,0 +1,13 @@
+//
+//  BackgroundSceneView.m
+//  MYFlappyBird
+//
+//  Created by 马遥 on 15/12/28.
+//  Copyright © 2015年 Mervin. All rights reserved.
+//
+
+#import "BackgroundSceneView.h"
+
+@implementation BackgroundSceneView
+
+@end
